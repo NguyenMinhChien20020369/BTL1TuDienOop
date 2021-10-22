42d9fdd2673aa592e03cf967efc5616feb595464
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2021-10-22 15:24:52 +0700

    Update file txt

---
+++ src/main/java/Overall/Description.java
@@ -0,0 +1,29 @@
+package Overall;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Description {
+    private String definition;
+    private  ArrayList<String> example;
+    public Description(){
+        definition = new String();
+        example = new ArrayList<>();
+    }
+
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public void setExample(String example) {
+        this.example.add(example);
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+    public ArrayList<String> getExample() {
+        return example;
+    }
+}
